e4dd780216013fb187590eda448d20c0d56e837d
generic-name-2166 <kirbesson277@mail.ru>
2025-04-18 16:32:32 +0300

    feat: filtering by dates

--- server/api/data.get.ts
+++ server/api/data.get.ts
@@ -1,23 +1,23 @@
 interface Product {
   id: number;
   status: string;
-  date_created: Date;
+  date_created: number;
   name: string;
   price: number;
 }
 
 export default defineEventHandler(async () => {
   const data: Product[] = [
-    { id: 0, status: "OK", date_created: new Date(), name: "a", price: 0 },
-    { id: 1, status: "OK", date_created: new Date(), name: "a", price: 0 },
-    { id: 2, status: "OK", date_created: new Date(), name: "a", price: 0 },
-    { id: 3, status: "OK", date_created: new Date(), name: "a", price: 0 },
-    { id: 4, status: "OK", date_created: new Date(), name: "a", price: 0 },
-    { id: 5, status: "OK", date_created: new Date(), name: "a", price: 0 },
-    { id: 6, status: "OK", date_created: new Date(), name: "a", price: 0 },
-    { id: 7, status: "OK", date_created: new Date(), name: "a", price: 0 },
-    { id: 8, status: "OK", date_created: new Date(), name: "a", price: 0 },
-    { id: 9, status: "OK", date_created: new Date(), name: "a", price: 0 },
+    { id: 0, status: "OK", date_created: Date.now() + 1, name: "a", price: 0 },
+    { id: 1, status: "OK", date_created: Date.now() + 2, name: "a", price: 0 },
+    { id: 2, status: "OK", date_created: Date.now() + 3, name: "a", price: 0 },
+    { id: 3, status: "OK", date_created: Date.now() + 4, name: "a", price: 0 },
+    { id: 4, status: "OK", date_created: Date.now() + 5, name: "a", price: 0 },
+    { id: 5, status: "OK", date_created: Date.now() + 6, name: "a", price: 0 },
+    { id: 6, status: "OK", date_created: Date.now() + 7, name: "a", price: 0 },
+    { id: 7, status: "OK", date_created: Date.now() + 8, name: "a", price: 0 },
+    { id: 8, status: "OK", date_created: Date.now() + 9, name: "a", price: 0 },
+    { id: 9, status: "OK", date_created: Date.now() + 10, name: "a", price: 0 },
   ];
   return data;
 });
--- src/pages/account.vue
+++ src/pages/account.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
-/* interface Product {
+interface Product {
   id: number;
   status: string;
   date_created: Date;
   name: string;
-  price: bigint;
-} */
+  price: number;
+}
 
 definePageMeta({
   middleware: [
@@ -21,12 +21,54 @@ definePageMeta({
 const router = useRouter();
 
 const { data } = await useFetch("/api/data");
+type Products = typeof data;
 
 const click = (): void => {
   const cookie = useCookie("token");
   cookie.value = "";
   router.push("/");
 };
+
+interface FilterView {
+  status: string;
+  date_created: number[];
+  name: string;
+}
+
+const filterObj = ref<FilterView>({ status: "", date_created: [], name: "" });
+
+const setDate = (event: Event) => {
+  const options = (event.target as HTMLSelectElement).selectedOptions;
+  filterObj.value.date_created = Array.from(options).map((option) =>
+    parseInt(option.value),
+  );
+};
+
+function filterView(data: Products, filterObj: FilterView): Product[] {
+  return (
+    data.value
+      ?.filter((row) => {
+        if (filterObj.status.length > 0 && row.status !== filterObj.status) {
+          return false;
+        } else if (
+          filterObj.date_created.length > 0 &&
+          !filterObj.date_created.includes(row.date_created)
+        ) {
+          return false;
+        }
+        return filterObj.name.length === 0 || row.name.includes(filterObj.name);
+      })
+      .map((row) => ({
+        id: row.id,
+        status: row.status,
+        date_created: new Date(row.date_created),
+        name: row.name,
+        price: row.price,
+      })) ?? []
+  );
+}
+
+const view = computed(() => filterView(data, filterObj.value));
 </script>
 
 <template>
@@ -34,7 +76,21 @@ const click = (): void => {
     <header>
       <button type="button" @click="click">Log out</button>
     </header>
-    <main>
+    <main :class="$style.main">
+      <div>
+        <div :class="$style.filter">
+          <label>Choose dates</label>
+          <select multiple size="5" @change="setDate">
+            <option
+              v-for="date in data?.map((row) => new Date(row.date_created))"
+              :key="date.valueOf()"
+              :value="date.valueOf()"
+            >
+              {{ date }}
+            </option>
+          </select>
+        </div>
+      </div>
       <table :class="$style.table">
         <thead>
           <tr>
@@ -46,7 +102,7 @@ const click = (): void => {
           </tr>
         </thead>
         <tbody>
-          <tr v-for="row in data" :key="row.id">
+          <tr v-for="row in view" :key="row.id">
             <td :class="$style.td">{{ row.id }}</td>
             <td :class="$style.td">{{ row.status }}</td>
             <td :class="$style.td">{{ row.date_created }}</td>
@@ -65,6 +121,16 @@ const click = (): void => {
   flex-direction: column;
   align-items: center;
   justify-content: center;
+}
+
+.filter {
+  display: flex;
+  flex-direction: column;
+}
+
+.main {
+  display: flex;
+  flex-direction: column;
 }
 
 .table {
